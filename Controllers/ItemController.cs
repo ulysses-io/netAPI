d1154a55556b31e3c9772edd0573a80850ba1fa7
ulysses-io <grant.swalwell-1@ou.edu>
2020-11-23 15:36:13 -0600

    starter commit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using netAPI.Models;
+
+namespace JsonWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly ItemContext _context;
+
+        public ItemController(ItemContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> GetItems()
+        {
+            return await _context.Items.ToListAsync();
+        }
+
+
+        [HttpGet("{ID}")]
+        public async Task<ActionResult<Item>> GetItem(int ID)
+        {
+            var item = await _context.Items.FindAsync(ID);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Item>> PostItem(Item item)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetItem), new { id = item.ID }, item);
+        }
+    }
+}
+
+
+
+
+
---
+++ Models/ItemContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace netAPI.Models
+{
+    public class ItemContext : DbContext
+    {
+
+        public ItemContext(DbContextOptions<ItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }    
+}
+
+
+
